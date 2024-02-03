6077200594a716843097d1cbb49f2fc06ebeb78a
OuattaraFatogoma <ouattarafatogmaabdoulaziz@gmail.com>
2024-02-03 10:28:35 +0100

    add notFound and errorHandler middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,36 @@
 require('dotenv').config();
+require('express-async-errors');
 const express = require('express');
 const mongoose = require('mongoose');
+
 //import routes
 const usersRoute = require('./routes/users');
 const productsRoute = require('./routes/products');
 const adminsRoute = require('./routes/admins');
 const cartsRoute = require('./routes/carts');
 const invoicesRoute = require('./routes/invoices');
-
+// import middlwares 
+const notFoundMiddleware = require('./middleware/notFound');
+const errorHandlerMiddleware = require('./middleware/errorHandler');
 
 const app = express();
 const port = process.env.PORT || 5000
+
+
 // middlewares
 app.use(express.json());
 
 
 //routes
+
 app.use('/api/v1/users', usersRoute);
 app.use('/api/v1/products', productsRoute);
 app.use('/api/v1/admins', adminsRoute);
 app.use('/api/v1/carts', cartsRoute);
 app.use('/api/v1/invoices', invoicesRoute);
 
+app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware);
 
 
 //Connect database and listen at port
